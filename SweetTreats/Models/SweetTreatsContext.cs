9d2f0a7364ef72dd774f4d3b0cb9f156dfff8f90
Rachel Schieferstein <violenza@gmail.com>
2020-03-27 08:56:15 -0700

    adding context and user models

---
+++ SweetTreats/Models/SweetTreatsContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetTreats.Models
+{
+  public class SweetTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+    
+    public SweetTreatsContext(DbContextOptions options) : base(options) { }
+  }
+} 
