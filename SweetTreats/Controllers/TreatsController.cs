dbdfbffd17980a684246f6ff7d289ea14c451ac8
Rachel Schieferstein <violenza@gmail.com>
2020-03-27 12:11:24 -0700

    adding edit route and view for treat

--- SweetTreats/Controllers/TreatsController.cs
+++ SweetTreats/Controllers/TreatsController.cs
@@ -56,6 +56,24 @@ namespace SweetTreats.Controllers
         .FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
+    public ActionResult Edit(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(Treats => Treats.TreatId == id);
+      ViewBag.AuthorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.Entry(treat).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }
